6257424b90211799ddee2e1db7e4b66f0f4b0c0e
Dayvson Silva-DAYVSONS <dayvsons@motorola.com>
2021-06-04 12:44:34 -0400

    Two arrays and swaps

---
+++ algoritmos-gulosos/two-arrays-and-swaps.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    while (t--) {
+        int n, k; cin >> n >> k;
+        vector<int> a(n), b(n);
+        for (int i=0;i<n;i++) cin >> a[i];
+        for (int i=0;i<n;i++) cin >> b[i];
+        sort(a.begin(), a.end()); sort(b.begin(), b.end());
+        for (int i=0;i<k;i++){
+            if (a[i] < b[n-1-i]){
+                swap(a[i], b[n-1-i]);
+            }
+        }
+        cout << accumulate(a.begin(), a.end(), 0) << endl;
+
+    }
+}
