96757e50342d0ff53171bfd42f1761d2fb4ebb7a
Dayvson Silva-DAYVSONS <dayvsons@motorola.com>
2021-07-29 01:11:35 -0400

    Closest to the Right

---
+++ 04-arvores-binarias-busca/closes_right.cpp
@@ -0,0 +1,37 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+vector <ll> a;
+
+int p(ll idx, ll x){
+    return x <= a[idx];
+}
+
+int bsearch(ll lo, ll hi, ll x){
+    ll md;
+    while(lo < hi){
+        md = lo + (hi-lo)/2;
+        if(p(md, x)) hi = md;
+        else lo = md + 1;
+    }
+    return lo; 
+}
+
+int main(){
+    ll n, k, ki, ic;
+    cin >> n >> k;
+    for(int i = 0; i < n; i++){
+        cin >> ki;
+        a.push_back(ki);
+    }
+    for(int i = 0; i < k; i++){
+        cin >> ki;
+        ic = bsearch(0, n-1, ki);
+        if(a[ic] < ki) cout << n+1 << endl;
+        else cout << ic+1 << endl;
+    }
+    return 0;
+}
